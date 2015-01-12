1853314c0d3cf1ec613d43dc67c25e78e8375a13
kevin-MAC <kevin@kevin-MACdeMacBook-Pro.local>
2015-01-12 18:37:16 +0800

    add

--- MI.py
+++ MI.py
@@ -1,7 +1,8 @@
+#-*- coding:utf-8 -*-
 import matplotlib.pyplot as plt
-from scipy import misc
+#from scipy import misc
 import gdal
-from skimage.feature import corner_harris, corner_subpix, corner_peaks
+#from skimage.feature import corner_harris, corner_subpix, corner_peaks
 from skimage import filter
 import numpy as np
 from numpy import zeros
@@ -9,7 +10,7 @@ from numpy import array
 from numpy import log2
 from numpy import double
 from numpy.lib.function_base import histogram
-from numpy import argmax
+#from numpy import argmax
 def CalMI(a,b):
 
     reference = array(a)
@@ -41,7 +42,6 @@ def CalMI(a,b):
     NMI = 2*MI/(r+q)
     #print(r,q,MI)
     return NMI
-
 # Generate noisy image of a square
 # im = np.zeros((128, 128))
 # im[32:-32, 32:-32] = 1
@@ -50,24 +50,31 @@ def CalMI(a,b):
 sards = gdal.Open('SAR.tif')
 imsar = sards.ReadAsArray()
 imsar = np.double(imsar)
+imsar_ag = np.zeros((imsar.shape),dtype = float)
 
 opds = gdal.Open('op.tif')
 imop=opds.ReadAsArray()
 imop = np.double(imop)
+imop_age = np.zeros((imop.shape),dtype=float)
+
 
 #Get Harris Point from optical Image
 ds = gdal.Open('op.tif')
 image = ds.ReadAsArray()
-coords = corner_peaks(corner_harris(image), min_distance=9)
-poss = zip(coords[:,0],coords[:,1])
+#coords = corner_peaks(corner_harris(image), min_distance=9)
+#poss = zip(coords[:,0],coords[:,1])
 # im = im/np.max(im)
 # im = ndimage.rotate(im, 15, mode='constant')
-# im = ndimage.gaussian_filter(im, 4)
+#im = ndimage.gaussian_filter(im, 4)
 # im += 0.2 * np.random.random(im.shape)
 # Compute the Canny filter for two values of sigma
 edges1 = filter.canny(imop,sigma = 5)
-edges2 = filter.canny(imsar, sigma=5)
-sarx,sary = edges2.shape
+edges2 = filter.canny(imsar,sigma= 5)
+
+
+
+filter.gaussian_filter()
+sarX,sarY = edges2.shape
 #Find Image Match Point
 ws = 11
 ij=[]
@@ -85,26 +92,25 @@ ij=[]
 #     u = np.argmax(np.array(tempNmi))
 #     ij.append(tempij[u])
 # display results
-fig, (ax1, ax2)  = plt.subplots(nrows=1, ncols=2, figsize=(8, 3))
+#fig, (ax1, ax2)  = plt.subplots(nrows=1, ncols=2, figsize=(8, 3))
 
 # ax1.imshow(imop, cmap=plt.cm.gray)
 # ax1.axis('off')
 # ax1.set_title('noisy image', fontsize=20)
 
-ax2.imshow(edges1, cmap=plt.cm.gray)
-ax2.axis('off')
-ax2.set_title('Op, $\sigma=5$', fontsize=20)
+#ax2.imshow(edges1, cmap=plt.cm.gray)
+#ax2.axis('off')
+#ax2.set_title('Op, $\sigma=5$', fontsize=20)
 # ax2.plot(poss[1][0],ij[1][1],'+r',markersize = 15)
 
-ax1.imshow(edges2, cmap=plt.cm.gray)
-ax1.axis('off')
-ax1.set_title('Sar, $\sigma=5$', fontsize=20)
+#ax1.imshow(edges2, cmap=plt.cm.gray)
+#ax1.axis('off')
+#ax1.set_title('Sar, $\sigma=5$', fontsize=20)
 # ax3.plot(ij[0][0],ij[0][1],'+r',markersize = 15)
 
-fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
-                    bottom=0.02, left=0.02, right=0.98)
+#fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9, bottom=0.02, left=0.02, right=0.98)
 
 
-plt.show()
+#plt.show()
 # plt.savefig('sar_op_edges5.png',dpi = 500)
 
