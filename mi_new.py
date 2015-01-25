bd8a25c8a3caa4d2bf082acdb69f13bbcfcdaf88
kevin-MAC <kevin@kevin-MACdeMacBook-Pro.local>
2015-01-26 00:29:52 +0800

    add

--- mi_new.py
+++ mi_new.py
@@ -3,7 +3,8 @@ import matplotlib.pylot as plt
 import gdal
 from skimage import filter
 import numpy as np
-
+# Add the new line
+# Add the second line
 from numpy.lib.function_base import histogram
 
 def CalMI(a,b):
@@ -42,6 +43,7 @@ def CalMI(a,b):
 
 def CalCC(a,b):
     """
+
     Calculate the CC
     """
     CC = 0.0
@@ -56,28 +58,3 @@ def GetRectBuffer(edge,width):
     width: Buffer's width
     """
     return edge
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
